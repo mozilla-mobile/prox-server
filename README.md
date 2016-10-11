b67c426f6d0c511a62ed275dcb49a3ebb71fadd6
Chenxia Liu <liuche@mozilla.com>
2016-10-10 19:17:20 -0700

    Add requirements.txt, Firebase integration, and update README.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # prox-server
 This is the server side component to the first NMX prototype project.
+
+# Setup
+Set up a virtualenv (assuming you already installed virtualenv with pip) and install dependencies.
+
+    virtualenv env
+    source env/bin/activate
+    pip install -r requirements.txt
+
+and `deactivate` to leave the virtualenv.
+
+## Firebase
+Add the path to the Firebase credentials json file to `config.py`, and then run
+
+    python firebase-test.py
+
+You can view real-time changes to the database on the console.
---
+++ config.py
@@ -0,0 +1,14 @@
+# Paths to key files
+FIREBASE_KEY_PATH="/Users/liuche/moz/service-keys/prox-server-c1.json"
+
+# Firebase config for prox-server
+# These are public-facing and can be found in the console under Auth > Web Setup (in the top-right corner)
+FIREBASE_CONFIG = {
+    "apiKey": "AIzaSyCksV_AC0oB9OnJmj0YgXNOrmnJawNbFeE",
+    "authDomain": "prox-server-cf63e.firebaseapp.com",
+    "databaseURL": "https://prox-server-cf63e.firebaseio.com",
+    "storageBucket": "prox-server-cf63e.appspot.com",
+    "messagingSenderId": "888537898788",
+    "serviceAccount": FIREBASE_KEY_PATH
+    # Using the service account will authenticate as admin by default
+}

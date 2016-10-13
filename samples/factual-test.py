09b0de715a3475b0311571b5d7f98867e62a4306
James Hugman <james@hugman.tv>
2016-10-13 15:39:08 +0100

    Crosswalk resolution of additional API identifiers. This is the simplest thing that could possibly work.

--- samples/factual-test.py
+++ samples/factual-test.py
@@ -22,3 +22,22 @@ factualIDs = [ p["factual_id"] for p in factualPlaces ]
 idArray = fetchCrosswalk(factualIDs)
 print(json.dumps(idArray, indent=4))
 #fetchCrosswalk([1,2,3])
+
+def findFactualIDs(yelpBusinesses):
+    totalFilter = {
+      "$or": [ {"url": {"$eq": "https://yelp.com/biz/%s" % biz.id }} for biz in yelpBusinesses]
+    }
+    crosswalkObjects = factualClient.crosswalk().filters(totalFilter).data()
+    return [biz["factual_id"] for biz in crosswalkObjects]    
+
+#factualIDs = findFactualIDs(businesses)
+
+def fetchCrosswalk(factualIDs):
+    namespaces = ["tripadvisor", "wikipedia", "facebook"]
+    oneOfFilter = [{ "factual_id": {"$eq": f}} for f in factualIDs]
+    idFilter = { "$or": oneOfFilter }
+    oneOfFilter = [ { "namespace": {"$eq": ns }} for ns in namespaces]
+    namespaceFilter = { "$or": oneOfFilter }
+    totalFilter = { "$and": [idFilter, namespaceFilter]}
+    return factualClient.crosswalk().filters(totalFilter).data()
+

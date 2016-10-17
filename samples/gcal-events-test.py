409e45b40cfb35e0fb839121077be28ea6dad539
Chenxia Liu <liuche@mozilla.com>
2016-10-17 14:52:38 -0700

    Cleanup.

--- samples/gcal-events-test.py
+++ samples/gcal-events-test.py
@@ -2,7 +2,6 @@ import datetime
 from pprint import pprint as pp
 import re
 import requests
-import usaddress
 
 from app.clients import yelpClient
 from app.clients import googleapikey
@@ -11,13 +10,14 @@ CAL_ID = 'hawaii247.com_dm8m04hk9ef3cc81eooerh3uos@group.calendar.google.com'
 CALENDAR_URL = 'https://www.googleapis.com/calendar/v3/calendars/{}/events?key={}'
 KONA_LAT_LONG = '19.622345,-155.665041'
 KONA_RADIUS = 100000
+DEFAULT_COUNTRY = 'country:US'
 
 '''
   Best effort to fetch event details (including a Yelp id) from a Google calendar.
   Returns a list of event JSON, each of the form:
     { 'yelp_id':
       'location':
-      'event_name':
+      'event_summary':
       'start_time':
       'url':
     }
@@ -31,101 +31,106 @@ def fetchEvents(calId, maxResults, timeRangeMs):
     now_rfc = getRfcTime(now)
     later_rfc = getRfcTime(now + timeRangeMs)
 
-    params = {'orderBy': 'startTime', \
-              'singleEvents': True, \
-              'timeMin': now_rfc, \
-              'timeMax': later_rfc, \
+    eventParams = {'orderBy': 'startTime',
+              'singleEvents': True,
+              'timeMin': now_rfc,
+              'timeMax': later_rfc,
               'maxResults': maxResults }
 
-    r = requests.get(CALENDAR_URL.format(CAL_ID, googleapikey), params)
+    r = requests.get(CALENDAR_URL.format(CAL_ID, googleapikey), eventParams)
     items = r.json()['items']
     return items
 
 def getRfcTime(utcTime):
     return utcTime.isoformat('T') + 'Z'
 
-def getLocationInfo(itemLocation, summary):
-    name, address = getNameAndAddress(itemLocation)
-
+def getEventObj(event):
     # Check address, then name, then summary
+    name, address = getNameAndAddress(event['location'])
+    summary = event['summary']
+    print(name + '_' + address + '_' + summary)
 
     # Check address first for lat/long
     if address:
-        print('===address===')
         try:
             ## address => geocode => textsearch.name => Yelp(name, location)
-            params = { 'address': address, 'key': googleapikey, 'components': 'country:US' }
+            params = { 'address': address,
+                       'key': googleapikey,
+                       'components': DEFAULT_COUNTRY }
+
             r = requests.get('https://maps.googleapis.com/maps/api/geocode/json', params)
             results = r.json()['results']
 
-            if len(results) > 1:
-                result = results[0]
-                placeId = result['place_id']
-                location = result['geometry']['location']
+            if len(results) > 0:
+                placeId = results[0]['place_id']
+                location = results[0]['geometry']['location']
 
-                params = { 'location': str(location['lat']) + ',' + str(location['lng']), \
-                           'radius': 1,
-                           'query': summary,
-                           'key': googleapikey }
+                params = { 'query': summary,
+                           'key': googleapikey,
+                           'location': str(location['lat']) + ',' + str(location['lng']),
+                           'radius': 5 }
 
                 ## Use textsearch
                 r = requests.get('https://maps.googleapis.com/maps/api/place/textsearch/json', params)
                 results = r.json()['results']
 
                 if len(results) > 0:
-                    placeName = results[0]['name']
-                    r = yelpClient.search_by_coordinates(location['lat'], location['lng'], \
-                                                         limit=1,
-                                                         term=placeName)
-                    b = r.businesses
-                    if len(b) > 0:
-                        return (b[0].id, location)
+                    eventObj = getYelpId(results[0]['name'], location)
+                    if (eventObj):
+                        return eventObj
 
         except Exception as err:
-            print('Google Geocaching API: Couldn\'t find item, error: {}'.format(err))
+            print('Searching by address: error: {}'.format(err))
 
     # Search for location by name
     if name:
-        print('==name==')
-        params = { 'query': name, \
-                   'location': KONA_LAT_LONG, \
-                   'radius': 5000, \
-                   'key': googleapikey }
+        try:
+            params = { 'query': name,
+                       'key': googleapikey,
+                       'location': KONA_LAT_LONG,
+                       'radius': 5000 }
 
-        r = requests.get('https://maps.googleapis.com/maps/api/place/textsearch/json', params)
-        results = r.json()['results']
-        if len(results) > 0:
-            placeName = results[0]['name']
-            location = results[0]['geometry']['location']
+            r = requests.get('https://maps.googleapis.com/maps/api/place/textsearch/json', params)
+            results = r.json()['results']
 
-            # Find yelp id from name and location
-            r = yelpClient.search_by_coordinates(location['lat'], location['lng'], \
-                                             limit=1, \
-                                             term=placeName)
-            b = r.businesses
-            if len(b) > 0:
-                return (b[0].id, location)
+            if len(results) > 0:
+                placeName = results[0]['name']
+                location = results[0]['geometry']['location']
+
+                eventObj = getYelpId(placeName, location)
+                if (eventObj):
+                    return eventObj
+
+        except Exception as err:
+            print('Searching by name, error: {}'.format(err))
 
     # Search by summary
-    print('==radar==')
-    params = { 'keyword': summary, \
-               'location': KONA_LAT_LONG, \
-               'radius': KONA_RADIUS, \
+    params = { 'keyword': summary,
+               'location': KONA_LAT_LONG,
+               'radius': KONA_RADIUS,
                'key': googleapikey }
 
     try:
         r = requests.get('https://maps.googleapis.com/maps/api/place/radarsearch/json', params)
         results = r.json()['results']
+        #pp(results)
         if len(results) > 0:
             result = results[0]
             placeId = result['place_id']
-            params = { 'placeid': placeId, \
+
+            params = { 'placeid': placeId,
                        'key': googleapikey }
-            res = requests.get('https://maps.googleapis.com/maps/api/place/details/json', params)
-            return res.json()['result']['name']
+
+            result = requests.get('https://maps.googleapis.com/maps/api/place/details/json', params).json()['result']
+            placeName = result['name']
+            location = result['geometry']['location']
+
+            eventObj = getYelpId(placeName, location)
+            if (eventObj):
+                return eventObj
 
     except Exception as err:
-        print('PROBLEM: {}'.format(err))
+        print('Searching by summary, error: {}'.format(err))
 
 
 nameAddressRegex = re.compile('^([^0-9]*)([0-9]*.*)')
@@ -137,8 +142,16 @@ def getNameAndAddress(rawLocation):
     regex = re.search(nameAddressRegex, output) # guess at splitting into (name, address) fields if possible
     return regex.groups()
 
-eventsList = fetchEvents(CAL_ID, 15, datetime.timedelta(days=7))
+def getYelpId(placeName, locationObj):
+    # Find yelp id from name and location
+    r = yelpClient.search_by_coordinates(locationObj['lat'], locationObj['lng'], term=placeName[:30], limit=1, radius_limit=5)
+
+    b = r.businesses
+    if len(b) > 0:
+        return b[0].id
+
+eventsList = fetchEvents(CAL_ID, 5, datetime.timedelta(days=10))
 for event in eventsList:
     if 'location' in event:
-        pp(getLocationInfo(event['location'], event['summary']))
+        pp(getEventObj(event))
         print('\n')

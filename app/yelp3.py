ce1bbf05c2a2d08fd3c3e3c254ee9e9628729207
James Hugman <james@hugman.tv>
2016-10-19 23:22:52 +0100

    Swapped yelp.get_business API call for yelp3.businesses API.

---
+++ app/yelp3.py
@@ -0,0 +1,32 @@
+from sanction import Client
+
+class Yelp3Client:
+    def __init__(self, client_id, client_secret):
+        self.client  = Client(
+          token_endpoint="https://api.yelp.com/oauth2/token", 
+          resource_endpoint="https://api.yelp.com/v3", 
+          client_id=client_id, 
+          client_secret=client_secret
+        )
+
+    def refreshAccessToken(self):
+        if self.client.access_token is not None:
+            from datetime import datetime, timedelta
+            from time import mktime
+            now = mktime(datetime.utcnow().timetuple())
+            if now <= self.client.token_expires:
+                return self.client.access_token  
+
+        self.client.request_token(grant_type="client_credentials")
+        return self.client.access_token
+
+    def request(self, endpoint):
+        access_token = self.refreshAccessToken()
+        return self.client.request(endpoint, 
+            method="GET", 
+            headers={'Authorization': 'Bearer {0}'.format(access_token)}
+        )
+
+
+
+
--- samples/yelp-test.py
+++ samples/yelp-test.py
@@ -1,21 +1,26 @@
 import json
 import pprint
 
-from app.clients import yelpClient, factualClient
+from app.clients import yelpClient, yelp3Client
 from app.representation import venueRecord
 
-categories="beaches"
+# categories="beaches"
 
-def getLocality(lat, lon, **kwargs):
-    return yelpClient.search_by_coordinates(lat, lon, **kwargs)
+# def getLocality(lat, lon, **kwargs):
+#     return yelpClient.search_by_coordinates(lat, lon, **kwargs)
 
-locality = getLocality(19.915403, -155.887403, 
-  radius_filter=25000, 
-  sort=1, 
-  limit=20, 
-  offset=0, 
-  category_filter=categories
-)
+# locality = getLocality(19.915403, -155.887403, 
+#   radius_filter=25000, 
+#   sort=1, 
+#   limit=20, 
+#   offset=0, 
+#   category_filter=categories
+# )
 
-businesses = locality.businesses
-print(json.dumps([venueRecord(b) for b in businesses], indent=2))
+# businesses = locality.businesses
+# print(json.dumps([venueRecord(b) for b in businesses], indent=2))
+
+#yelpID = "north-india-restaurant-san-francisco"
+yelpID = "holoholokai-beach-park-waimea"
+yelp3Biz = yelp3Client.request("/businesses/%s" % yelpID)
+print(json.dumps(yelp3Biz, indent=2))

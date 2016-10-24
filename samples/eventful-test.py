4fa87e5b448a0955d577caa4d9b78c33399bf932
Chenxia Liu <liuche@mozilla.com>
2016-10-24 14:51:27 -0700

    Add eventful sample.

--- samples/eventful-test.py
+++ samples/eventful-test.py
@@ -1,5 +1,5 @@
+import json
 import requests
-from pprint import pprint as pp
 
 import app.events as events
 
@@ -8,7 +8,10 @@ SF_LATLONG = "37.789225, -122.389521"
 NUM_RESULTS = 10
 
 eventsList = events.fetchEventsFromLocation(SF_LATLONG, NUM_RESULTS, 2)
+eventObjList = []
 for event in eventsList:
     eventObj = events.getEventfulEventObj(event)
     if eventObj:
-        pp(eventObj)
+        eventObjList.append(eventObj)
+
+print(json.dumps(eventObjList, indent=2))

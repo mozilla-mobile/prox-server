3b2874b84c64a4c11f8aa58faaed593c2c78fd61
James Hugman <james@hugman.tv>
2016-10-31 14:58:51 +0000

    Add constants around caching searches.

--- app/constants.py
+++ app/constants.py
@@ -4,7 +4,13 @@
 _tablePrefix = ""
 _venues = "venues"
 _events = "events"
+_searches = "searches"
+
+# number of seconds after a search in a given area that it should be cached.
+searchCacheExpiry = 60 * 60 * 24 # s
+searchCacheRadius = 1000 # m
 
 # Don't edit these directly.
 venuesTable = _tablePrefix + _venues
 eventsTable = _tablePrefix + _events
+searchesTable = _tablePrefix + _searches

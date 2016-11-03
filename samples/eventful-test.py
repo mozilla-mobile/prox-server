d8521d1c6c4ae91d3cc158a1ae87a556d85aa812
Chenxia Liu <liuche@mozilla.com>
2016-11-03 16:54:13 -0700

    Add sample for gcal.

--- samples/eventful-test.py
+++ samples/eventful-test.py
@@ -4,10 +4,10 @@ import requests
 import app.events as events
 
 EVENTFUL_URL = 'https://api.eventful.com/json/events/search'
-SF_LATLONG = "37.789225, -122.389521"
+KONA_LATLONG = "19.924301, -155.887519"
 NUM_RESULTS = 10
 
-eventsList = events.fetchEventsFromLocation(SF_LATLONG, NUM_RESULTS, 2)
+eventsList = events.fetchEventsFromLocation(KONA_LATLONG, NUM_RESULTS, 17, "2016120500-2016120900")
 eventObjList = []
 for event in eventsList:
     eventObj = events.getEventfulEventObj(event)
--- samples/fetch-gcal-test.py
+++ samples/fetch-gcal-test.py
@@ -1,17 +1,19 @@
 import datetime
 import app.events as events
 import app.request_handler as request_handler
-from pprint import pprint as pp
+import json
 
+'''
 def testGcalEventLoad():
     request_handler.loadCalendarEvents(30, datetime.timedelta(days=5))
 
 testGcalEventLoad()
+'''
 
-'''
-eventsList = events.fetchEventsFromGcal(CAL_ID, 45, datetime.timedelta(days=10))
-for event in eventsList:
-    if 'location' in event:
-        eventObj = events.getGcalEventObj(event)
-        pp(eventObj)
-'''
+CAL_ID = 'hawaii247.com_dm8m04hk9ef3cc81eooerh3uos@group.calendar.google.com'
+
+def fetchGcalEvents():
+    eventsList = events.fetchEventsFromGcal(CAL_ID, 1000, datetime.timedelta(days=5), datetime.datetime.strptime("2016-12-05", "%Y-%m-%d"))
+    return [ events.getGcalEventObj(event) for event in eventsList if 'location' in event ]
+
+print(json.dumps(fetchGcalEvents(), indent=2))

4fa59fbfa03b5abfefc64062f74a448a0383af2b
James Hugman <james@hugman.tv>
2016-12-01 18:13:46 +0000

    Use name + zip code for error messages,.

--- app/search.py
+++ app/search.py
@@ -119,9 +119,12 @@ def _getAddressIdentifiers(address):
     r = requests.get('https://maps.googleapis.com/maps/api/geocode/json', params)
     results = r.json()['results']
     if len(results) > 0:
+        r = results[0]
         mapping = {
-          "id": results[0]['place_id'],
-          "location": results[0]['geometry']['location']
+          "id": r['place_id'],
+          "location": r['geometry']['location'],
+          "name": r['address_components'][0]['short_name'],
+          "zipcode": r['address_components'][-1]['short_name'],
         }
         return mapping
     return None

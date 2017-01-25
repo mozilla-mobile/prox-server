4b60054228031a528f15826692cc2f16d2506cbf
Michael Comella <michael.l.comella@gmail.com>
2017-01-25 10:27:01 -0800

    Issue #25: Refactor yelp resolution so can do with key (not just url).

--- app/providers/yelp.py
+++ app/providers/yelp.py
@@ -5,7 +5,12 @@ from app.util import slug
 
 idPattern = re.compile("/([^/\?]*)(\?.*)?$")
 
-def resolve(idObj):
-    key = slug(idObj["url"])
+
+def resolve_with_key(key):
     key = key.encode('utf-8').strip()
     return yelp3Client.request("/businesses/{0}".format(key))
+
+
+def resolve(idObj):
+    key = slug(idObj["url"])
+    return resolve_with_key(key)
